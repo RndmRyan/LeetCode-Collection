f1d278c7fcded7f3f9917c522f88970a43af10ba
Rayyan M. Zia <125385098+RndmRyan@users.noreply.github.com>
2024-04-29 20:55:02 +0300

    Time: 7 ms (81.26%) | Memory: 13 MB (66.37%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.cpp
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -2,22 +2,23 @@ class Solution {
 public:
     string convert(string s, int numRows) 
     {
-if (numRows == 1 || numRows >= s.length()) return s; // No change needed for numRows == 1 or greater than length of s
+        if (numRows == 1 || numRows >= s.length()) 
+            return s;
+
+        vector<string> zigzag(numRows);
+        int row = 0;
+        bool goingDown = false;
     
-    std::vector<std::string> zigzag(numRows);
-    int row = 0;
-    bool goingDown = false;
+        for (char c : s) {
+            zigzag[row] += c;
+            if (row == 0 || row == numRows - 1) 
+                goingDown = !goingDown;
+            row += goingDown ? 1 : -1;
+        }
     
-    for (char c : s) {
-        zigzag[row] += c;
-        if (row == 0 || row == numRows - 1) goingDown = !goingDown; // Change direction if top or bottom row
-        row += goingDown ? 1 : -1; // Move to next row
-    }
-    
-    std::string word;
-    for (const std::string& str : zigzag) {
-        word += str;
-    }
+        string word;
+        for (const std::string& str : zigzag) 
+            word += str;
         return word;
     }
 };
