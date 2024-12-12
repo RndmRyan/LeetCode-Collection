c7a6a74c759a14ba7c26c268f4241a9c1826e894
Rayyan M. Zia <125385098+RndmRyan@users.noreply.github.com>
2024-12-12 17:42:20 +0300

    Time: 5 ms (92.11%) | Memory: 42.1 MB (39.54%) - LeetSync

--- 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.cs
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.cs
@@ -2,16 +2,13 @@ public class Solution {
     public long PickGifts(int[] gifts, int k) {
         for(int i=0; i<k; i++)
         {
-            int maxindex = 0, maxval = 0;
+            int maxindex = 0;
             for (int j=0; j<gifts.Length; j++)
             {
-                if(gifts[j]>maxval)
-                {
-                    maxval = gifts[j];
+                if(gifts[j]>gifts[maxindex])
                     maxindex = j;
-                }
             }
-            gifts[maxindex] = (int)Math.Floor(Math.Sqrt(maxval));
+            gifts[maxindex] = (int)Math.Floor(Math.Sqrt(gifts[maxindex]));
         }
         long sum=0;
         for(int i=0; i<gifts.Length;i++)
