e7658ed288a1cb2e3379f7dc26b7d7c0eb96a41b
Rayyan M. Zia <125385098+RndmRyan@users.noreply.github.com>
2025-07-15 12:23:41 +0500

    Time: 231 ms (29.58%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -1,5 +1,3 @@
 -- Write your PostgreSQL query statement below
 Select cs.name AS Customers from Customers cs
-WHERE cs.id not in (Select c.id from Customers c
-right join orders o on c.id = o.customerId
-group by c.id having(count(c.id)>0))
+WHERE cs.id not in (select customerId from orders)
